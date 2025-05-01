ac464e2ed6765d944792243ab16377dcfa3d8028
thiendang <thiendh310@gmail.com>
2025-05-01 23:53:55 +0700

    feat: add shared guards

---
+++ src/shared/guards/access-token.guard.ts
@@ -0,0 +1,35 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { REQUEST_USER_KEY } from 'src/shared/constants/auth.constant';
+import { TokenService } from 'src/shared/services/token.service';
+
+@Injectable()
+export class AccessTokenGuard implements CanActivate {
+  constructor(private readonly tokenService: TokenService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const accessToken = request.headers.authorization?.split(' ')[1];
+
+    if (!accessToken) {
+      throw new UnauthorizedException('Access token is required');
+    }
+
+    try {
+      const decodedAccessToken = await this.tokenService.verifyAccessToken(
+        accessToken as string,
+      );
+
+      request[REQUEST_USER_KEY] = decodedAccessToken;
+
+      return true;
+    } catch {
+      throw new UnauthorizedException();
+    }
+  }
+}
---
+++ src/shared/guards/api-key.guard.ts
@@ -0,0 +1,26 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import envConfig from 'src/shared/config';
+
+@Injectable()
+export class APIKeyGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+
+    const xAPIKey = request.headers['x-api-key'];
+
+    if (!xAPIKey) {
+      throw new UnauthorizedException('API key is required');
+    }
+
+    if (xAPIKey !== envConfig.SECRET_API_KEY) {
+      throw new UnauthorizedException('Invalid API key');
+    }
+
+    return true;
+  }
+}
