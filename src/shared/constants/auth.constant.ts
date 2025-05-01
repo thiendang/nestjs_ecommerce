22529323a8ceeaec18e2af22c6af1a3f0e1998a2
thiendang <thiendh310@gmail.com>
2025-05-01 23:53:14 +0700

    Add shared constant

---
+++ src/shared/constants/auth.constant.ts
@@ -0,0 +1,17 @@
+export const REQUEST_USER_KEY = 'user';
+
+export const AuthType = {
+  None: 'None',
+  Bearer: 'Bearer',
+  APIKey: 'APIKey',
+} as const;
+
+export type AuthTypeType = (typeof AuthType)[keyof typeof AuthType];
+
+export const ConditionGuard = {
+  And: 'And',
+  Or: 'Or',
+} as const;
+
+export type ConditionGuardType =
+  (typeof ConditionGuard)[keyof typeof ConditionGuard];
