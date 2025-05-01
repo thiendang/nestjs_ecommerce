723244b7df1869ed3847cde52fb6528ce5acbfbd
thiendang <thiendh310@gmail.com>
2025-05-01 23:53:37 +0700

    feat: add shared decorators

---
+++ src/shared/decorators/user.decorator.ts
@@ -0,0 +1,13 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { REQUEST_USER_KEY } from 'src/shared/constants/auth.constant';
+import { TokenPayload } from 'src/shared/types/jwt.type';
+
+export const User = createParamDecorator(
+  (field: keyof TokenPayload | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+
+    const token: TokenPayload | undefined = request[REQUEST_USER_KEY];
+
+    return field ? token?.[field] : token;
+  },
+);
